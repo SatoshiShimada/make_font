5e4336dd2262b0daa416b5562c043b35c8f22d81
Lotus <t62b85lin7xudc@i.softbank.jp>
2015-05-31 00:25:57 +0900

    Support comment. (lines starting with '#' will be ignored).

--- make_font.c
+++ make_font.c
@@ -8,31 +8,41 @@ int main(int argc, char *argv[])
 	unsigned short font;
 	int i, number = 0;
 
-	if(argc != 3) return -2;
+	if(argc != 3) {
+		fprintf(stderr, "Usage: %s [font source file name] [font output file name]\n", argv[0]);
+		return -2;
+	}
 
 	fin = fopen(argv[1], "r");
-	if(!fin) return -1;
+	if(!fin) {
+		fprintf(stderr, "Error: file open [%s]\n", argv[1]);
+		return -1;
+	}
 	fout = fopen(argv[2], "w");
-	if(!fout) return -1;
+	if(!fout) {
+		fprintf(stderr, "Error: file open [%s]\n", argv[2]);
+		return -1;
+	}
 
 	fprintf(fout, "unsigned short fontdata[] = {\n\t");
+	fprintf(fout, "/* %d */\n\t", number++);
 
 	do {
 		fgets(buf, sizeof(buf), fin);
 		if(buf[0] == '!') break;
+		if(buf[0] == '#') continue;
 		if(buf[0] == '\n') {
-			fprintf(fout, "\n\t/* %d */\n\t", number);
-			number++;
+			fprintf(fout, "\n\t/* %d */\n\t", number++);
 			continue;
 		}
 
 		for(i = 0; i < 16; i++) {
 			if(buf[i] == '@') {
-				font &= ~(0x01 << (15 - i));
+				font &= ~(0x0001 << (15 - i));
 			} else if(buf[i] == '.') {
-				font |= (0x01 << (15 - i));
+				font |= (0x0001 << (15 - i));
 			} else {
-				fprintf(stderr, "unknown charactor\n");
+				fprintf(stderr, "unknown charactor [%c]\n", buf[i]);
 			}
 		}
 		fprintf(fout, "0x%04x, ", font);
